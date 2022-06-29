4952511ac8ff584ec6778336f59d3bd1f40e76de
usefulsplash <emjavier16@gmail.com>
2022-06-29 16:28:36 -0400

    allow for multiple correct answers

--- khquiz.py
+++ khquiz.py
@@ -9,55 +9,74 @@ try:
 except ModuleNotFoundError:
     import tomli as tomllib
 
-NUM_QUESTIONS_PER_QUIZ = 5
+NUM_QUESTIONS_PER_QUIZ = 10
 QUESTIONS_PATH = pathlib.Path(__file__).parent / "questions.toml"
-QUESTIONS = tomllib.loads(QUESTIONS_PATH.read_text())
 
 
 def run_quiz():
-    questions = prep_questions(QUESTIONS, num_questions=NUM_QUESTIONS_PER_QUIZ)
+    questions = prep_questions(QUESTIONS_PATH, num_questions=NUM_QUESTIONS_PER_QUIZ)
 
     num_correct = 0
-    for num, (question, alternatives) in enumerate(questions, start=1):
+    for num, question in enumerate(questions, start=1):
         print(f"\nQuestion {num}:")
-        num_correct += ask_question(question, alternatives)
+        num_correct += ask_question(question)
 
     print(f"\nYou got {num_correct} correct out of {num} questions")
 
 
 # preprocessing -> preps QUESTIONS data structure to use for main loop
-def prep_questions(questions, num_questions):
+def prep_questions(path, num_questions):
+    questions = tomllib.loads(path.read_text())["questions"]
     num_questions = min(num_questions, len(questions))
-    return random.sample(list(questions.items()), k=num_questions)
+    return random.sample(questions, k=num_questions)
 
 
 # randomizes choices, calls get_answer for user input, then checks if they are correct or not
-def ask_question(question, alternatives):
-    correct_answer = alternatives[0]
+def ask_question(question):
+    correct_answers = question["answers"]
+    alternatives = question["answers"] + question["alternatives"]
     ordered_alternatives = random.sample(alternatives, k=len(alternatives))
 
-    answer = get_answer(question, ordered_alternatives)
-    if answer == correct_answer:
+    answer = get_answers(
+        question=question["question"],
+        alternatives=ordered_alternatives,
+        num_choices=len(correct_answers),
+    )
+    if set(answer) == set(correct_answers):
         print("Correct!")
         return 1
     else:
-        print(f"The answer is {correct_answer!r}, not {answer!r}")
+        is_or_are = " is" if len(correct_answers) == 1 else "s are"
+        print("\n- ".join([f"No, the answer{is_or_are}:"] + correct_answers))
         return 0
 
 
 # accepts question text and its choices, labels them, then returns user's answer
-def get_answer(question, alternatives):
+def get_answers(question, alternatives, num_choices=1):
     print(f"{question}?")
     labeled_alternatives = dict(zip(ascii_lowercase, alternatives))
     for label, alternative in labeled_alternatives.items():
         print(f"    {label}) {alternative}")
 
-    while (answer_label := input("\nYour Answer: ")) not in labeled_alternatives:
-        print(
-            f"Invalid choice, please use one of the following: {', '.join(labeled_alternatives)}"
-        )
+    while True:
+        plural_s = "" if num_choices == 1 else f"s (choose {num_choices})"
+        answer = input(f"\nYour answer{plural_s}: ")
+        answers = set(answer.replace(",", " ").split())
 
-    return labeled_alternatives[answer_label]
+        # handles invalid answers
+        if len(answers) != num_choices:
+            plural_s = "" if num_choices == 1 else "s, separated by comma"
+            print(f"Please answer {num_choices} alternative{plural_s}")
+            continue
+
+        if any((invalid := answer) not in labeled_alternatives for answer in answers):
+            print(
+                f"{invalid!r} is not a valid choice. "
+                f"Please use {', '.join(labeled_alternatives)}"
+            )
+            continue
+
+        return [labeled_alternatives[answer] for answer in answers]
 
 
 if __name__ == "__main__":
